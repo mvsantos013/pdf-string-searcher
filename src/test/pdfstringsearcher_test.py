0072ddc328b114c3f96908fd6905697063f8159f
mvsantos013 <38678223+mvsantos013@users.noreply.github.com>
2019-04-19 13:39:44 -0300

    adapt test

--- src/test/pdfstringsearcher_test.py
+++ src/test/pdfstringsearcher_test.py
@@ -4,14 +4,21 @@ import sys
 import unittest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pdf.pdfstringsearcher import PdfStringSearcher
+import utils.util as util
+import utils.network as network
 
 
 class TestPdfStringSearcher(unittest.TestCase):
 
     def test_class(self):
-        f = open("test.pdf", "rb")
+        """
+        Tests if pdf parser is working as expected
+        """
+        pdf_url = "http://www.africau.edu/images/default/sample.pdf"
+        response = network.get_response_from_url(pdf_url)
+        f = util.binary_to_file(network.extract_content_from_response(response))
         doc = PdfStringSearcher(f)
-        self.assertTrue(doc.contains_substring("professor"))
+        self.assertTrue(doc.contains_substring("Boring"))
         f.close()
 
 
