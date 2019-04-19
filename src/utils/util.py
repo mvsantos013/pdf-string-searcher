47e145d19ee5a770741b8049412bab2bc4f3e2fa
mvsantos013 <38678223+mvsantos013@users.noreply.github.com>
2019-04-19 04:26:59 -0300

    add url validation

--- src/utils/util.py
+++ src/utils/util.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import re
+
+regex_url = re.compile(
+        r'^(?:http|ftp)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
 def trim_whitespace(s):
@@ -20,3 +29,8 @@ def trim_whitespace(s):
         bf = bf + letter
 
     return bf.strip()
+
+
+def is_valid_url(url):
+    return re.match(regex_url, url) is not None
+
